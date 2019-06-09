28b24df911a3b32c94fa1b1e5d828a2e3450d6fc
JiPaix <jipai13@gmail.com>
2019-06-09 15:58:51 +0200

    added a mocha test

---
+++ test/index.js
@@ -0,0 +1,29 @@
+var assert = require('assert')
+let dotenvM = require('../index')
+
+describe('dotenvM', () => {
+    it('loads itself', () => {
+        assert.ok(dotenvM.constructor.name === "Env");
+    })
+    it('loads its dependencies', () => {
+        assert.equal('object', typeof dotenvM.fs)
+    })
+    describe('Methods', () => {
+        it('add()', () => {
+            dotenvM.add('MOCHA', true)
+            assert.ok(process.env.MOCHA)
+        })
+        it('del()', () => {
+            process.env.MOCHA = true;
+            dotenvM.del('MOCHA')
+            setTimeout(() => {
+                assert.equal('undefined', typeof process.env.MOCHA)
+            }, 250)
+        })
+        it('update()', () => {
+            dotenvM.add('MOCHA', false)
+            dotenvM.update('MOCHA', true)
+            assert.ok(process.env.MOCHA)
+        })
+    })
+})
