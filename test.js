d758ee4c4be4307e742c55bb186a7050e6f98b9b
JiPaix <jipai13@gmail.com>
2019-06-11 01:31:21 +0200

    in depth test (check file size after each changes)

--- test.js
+++ test.js
@@ -1,50 +1,43 @@
 var assert = require('assert')
 let dotenvM = require('./index')
 
-describe('dotenvM', () => {
-    it('loads itself', () => {
+describe('dotenv-manipulator', () => {
+    beforeEach((done) => {
+        setTimeout(() => {
+            done();
+        }, 10);
+    })
+    it('should load', () => {
         assert.ok(dotenvM.constructor.name === "Env");
     })
-    it('loads its dependencies', () => {
+    it('should have fs', () => {
         assert.equal('object', typeof dotenvM.fs)
     })
-    describe('Methods', () => {
-        it('add()', () => {
-            dotenvM._add('MOCHA', 'aribtrary value', () => {
-                if (process.env.MOCHA === 'aribtrary value' && dotenvM.fs.statSync('./.env').size == 24) {
-                    assert.ok(true)
-                } else if (dotenvM.fs.statSync('./.env').size !== 24) {
-                    assert.fail('something went wrong with the file')
-                } else if (process.env.MOCHA === 'aribtrary value') {
-                    assert.fail(`can't access process.env ADD`)
-                }
+
+    describe('#add()', () => {
+        it('should add a key with a value', () => {
+            dotenvM.add('MOCHA', 'arbitrary value', err => {
+                if (err) assert.fail(err)
+                assert.equal(dotenvM.fs.statSync('./.env').size, 23)
             })
         })
-        it('del()', () => {
-            process.env.MOCHA = 'arbitrary value';
-            dotenvM._del('MOCHA', () => {
-                if (typeof process.env.MOCHA === 'undefined' && dotenvM.fs.statSync('./.env').size == 0) {
-                    assert.ok(true)
-                } else if (dotenvM.fs.statSync('./.env').size !== 0) {
-                    assert.fail('something went wrong with the file')
-                } else if (typeof process.env.MOCHA !== 'undefined') {
-                    assert.fail(`can't access process.env ADD`)
-                }
+    })
+    describe('#update()', () => {
+        it('should update the value of a previously added key', () => {
+            assert.equal(dotenvM.fs.statSync('./.env').size, 23)
+            dotenvM.update('MOCHA', 'second arbitrary value that is super long', err => {
+                if (err) assert.fail(err)
+                assert.equal(51, dotenvM.fs.statSync('./.env').size)
             })
         })
-        it('update()', () => {
-            dotenvM._add('MOCHA', 'aribtrary value', () => {
-                dotenvM._del('MOCHA', () => {
-                    dotenvM._add('MOCHA', 'second aribtrary value', () => {
-                        if (process.env.MOCHA === 'second aribtrary value' && dotenvM.fs.statSync('./.env').size === 31) {
-                            assert.ok(true)
-                        } else if (dotenvM.fs.statSync('./.env').size !== 31) {
-                            assert.fail('something went wrong with the file')
-                        } else if (process.env.MOCHA === 'second aribtrary value') {
-                            assert.fail(`can't access process.env ADD`)
-                        }
-                    })
-                })
+    })
+
+    describe('#del()', () => {
+        it('should remove a key and leave the filesize at 0', () => {
+            assert.equal(51, dotenvM.fs.statSync('./.env').size)
+            dotenvM.del('MOCHA', err => {
+                if (err) assert.fail(err)
+                assert.equal(0, dotenvM.fs.statSync('./.env').size)
             })
         })
     })
