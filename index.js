8f9c2017ddd9adf345d7fb652373de62c8d7a247
JiPaix <jipai13@gmail.com>
2019-06-11 01:30:29 +0200

    added optional callbacks

--- index.js
+++ index.js
@@ -5,44 +5,113 @@ class Env {
         this.fs = require('fs')
         require('dotenv-expand')(require('dotenv').config())
     }
-    _add(key, value, callback) {
-        let prasedString = `${key.toUpperCase()}=${value}`;
-        let stream = this.fs.createWriteStream('./.env', { flags: 'a' })
-        process.env[key] = value
-        stream.write(`${prasedString} \r\n`, (err, written) => {
-            if (callback) {
-                callback();
-            }
-        })
-    }
-    _del(key, callback) {
-        this.fs.readFile('./.env', 'utf8', (err, fileContent) => {
-            if (err) throw err
-            let newFileContent = fileContent.replace(new RegExp(`(\r\n|\n|\r)?^${key}=.*(\r\n|\n|\r)*`, 'gim'), '')
-            let stream = this.fs.createWriteStream('./.env', { flags: 'w' })
-            delete process.env[key]
-            stream.write(newFileContent, () => {
-                if (callback) {
-                    callback();
+    add(key, value, callback) {
+        if (typeof process.env[key] === 'undefined') {
+            let fileContent;
+            this.fs.createReadStream('./.env').on('data', data => {
+                fileContent = data.toString()
+            }).on('close', () => {
+                if (typeof fileContent !== 'undefined') {
+                    if (!fileContent.match(new RegExp(`(\r\n|\n|\r)*^${key}=.*(\r\n|\n|\r)*`, 'gim'))) {
+                        let prasedString = `${key.toUpperCase()}=${value}\r\n`;
+                        let stream = this.fs.createWriteStream('./.env', { flags: 'a' })
+                        stream.write(prasedString)
+                        process.env[key] = value
+                        stream.close()
+                        if (typeof callback === 'function')
+                            stream.on('close', () => {
+                                callback();
+                            })
+                    } else {
+                        stream.close()
+                        if (typeof callback === 'function')
+                            stream.on('close', () => {
+                                callback(`this key already exists in .env file`)
+                            })
+                    }
+                } else {
+                    let prasedString = `${key.toUpperCase()}=${value}\r\n`;
+                    let stream = this.fs.createWriteStream('./.env', { flags: 'a' })
+                    stream.write(prasedString)
+                    stream.close()
+                    process.env[key] = value
+                    if (typeof callback === 'function')
+                        stream.on('close', () => {
+                            callback()
+                        })
                 }
             })
-        });
+        } else {
+            if (typeof callback === 'function')
+                callback(`this key already exists in process.env`)
+        }
     }
-    add(key, value) {
-        this._del(key, () => {
-            this._add(key, value)
-        })
-        process.env[key] = value;
+    del(key, callback) {
+        if (typeof process.env[key] !== 'undefined') {
+            let fileContent;
+            this.fs.createReadStream('./.env').on('data', data => {
+                fileContent = data.toString()
+            }).on('close', () => {
+                if (fileContent) {
+                    let newFileContent = fileContent.replace(new RegExp(`^${key}=.*(\r\n|\n|\r)*`, 'gim'), '')
+                    if (newFileContent !== fileContent) {
+                        let stream = this.fs.createWriteStream('./.env', { flags: 'w' })
+                        stream.write(newFileContent)
+                        stream.close();
+                        delete process.env[key]
+                        if (typeof callback === 'function')
+                            stream.on('close', () => {
+                                callback();
+                            })
+                    } else {
+                        if (typeof callback === 'function')
+                            stream.on('close', () => {
+                                callback(`key not found in .env file`)
+                            })
+                    }
+                } else {
+                    if (typeof callback === 'function')
+                        callback(`.env file is empty`)
+                }
+            })
+        } else {
+            if (typeof callback === 'function')
+                callback(`key not found in process.env`)
+        }
     }
-    update(key, value) {
-        this._del(key, () => {
-            this._add(key, value)
-        })
-        process.env[key] = value;
-    }
-    del(key) {
-        this._del(key, null)
+    update(key, value, callback) {
+        if (typeof process.env[key] !== 'undefined') {
+            let fileContent;
+            this.fs.createReadStream('./.env').on('data', data => {
+                fileContent = data.toString()
+            }).on('close', () => {
+                if (fileContent) {
+                    let prasedString = `${key.toUpperCase()}=${value}\r\n`;
+                    let newFileContent = fileContent.replace(new RegExp(`^${key}=.*`, 'gim'), prasedString)
+                    if (newFileContent !== fileContent) {
+                        let stream = this.fs.createWriteStream('./.env', { flags: 'w' })
+                        stream.write(newFileContent)
+                        stream.close();
+                        process.env[key] = value;
+                        if (typeof callback === 'function')
+                            stream.on('close', () => {
+                                callback()
+                            })
+                    } else {
+                        if (typeof callback === 'function')
+                            callback(`key not found in .env file`)
+                    }
+                } else {
+                    if (typeof callback === 'function')
+                        callback(`.env file is empty`)
+                }
+            })
+        } else {
+            if (typeof callback === 'function')
+                callback(`key not found in process.env`)
+        }
     }
 }
+
 const instance = new Env()
 module.exports = Object.freeze(instance)
